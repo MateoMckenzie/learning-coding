33b92c568de78225d45be799389193efcd4d12f5
MateoMckenzie <teiomckenzie@gmail.com>
2024-11-29 14:04:59 +0000

    Add files via upload
    assessment 2

---
+++ assessment 2 calc.py.py
@@ -0,0 +1,14 @@
+
+a = 123456
+b = 2346
+def multiply(value_a, value_b):
+    return value_a * value_b
+answer = multiply (b,a)
+print(answer)
+
+second_answer = multiply(answer, 120)
+print(second_answer)
+
+
+
+    
