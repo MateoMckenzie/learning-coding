b4c738a7b16abdc2354ac1c9c392aef699ba4149
MateoMckenzie <teiomckenzie@gmail.com>
2024-11-29 13:56:30 +0000

    assessment 3 and 4

---
+++ assessment 3.py
@@ -0,0 +1,7 @@
+decibels = float(input("how loud is the music?"))
+complaint = input("is the neighbour complaining?")
+
+if decibels >= 100 and complaint[0].lower()=="y":
+    print("turn the music down")
+
+    
---
+++ assessment 4 loops.py
@@ -0,0 +1,8 @@
+number = 1
+
+for i in range(number):
+    number = 1 * 1
+    print(1, end = ":")
+    print(number)
+
+number = 7
