cfe0fbaaac8f6cfa7d9f90b39bba49ae506a7247
MateoMckenzie <teiomckenzie@gmail.com>
2024-12-06 14:30:11 +0000

    Add files via upload
    assessment 6

---
+++ assessment 6.py
@@ -0,0 +1,11 @@
+numbers = 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9
+print ("1.List:", numbers)
+ 
+print ("2.Sum:", sum(numbers))
+
+print ("3.Largest number:", max(numbers))
+
+print ("4.Reverse order:", (numbers[::-1]))
+                    
+
+print ("5.Smallest number:", min(numbers))
